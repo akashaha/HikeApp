1fe0c94cda3f803e78f669b60f8c1880d93ea8b6
Arman Akash <armanakash@Armans-MacBook-Pro.local>
2023-07-17 09:56:42 +0600

    app icon changed feature is updated

--- Hike/Hike/Screen/SettingsView.swift
+++ Hike/Hike/Screen/SettingsView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    private let alternateAppIcons: [String] = [
+    "AppIcon-MagnifyingGlass","AppIcon-Map","AppIcon-Mushroom","AppIcon-Camera","AppIcon-Backpack","AppIcon-Campfire"
+    ]
     var body: some View {
         List {
             // MARK: - SECTION: HEADER
@@ -55,6 +60,49 @@ struct SettingsView: View {
             
             // MARK: -  SECTION: ICON
             
+            Section { ScrollView(.horizontal, showsIndicators: false) {
+                
+                
+                HStack (spacing: 12){
+                    ForEach(alternateAppIcons.indices, id: \.self) { item in
+                        Button {
+                            
+                            print("Icon \(alternateAppIcons[item]) was pressed")
+                            
+                            UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) { error in
+                                if error != nil{
+                                    print("Faied to update appicon: \(String(describing: error?.localizedDescription))")
+                                }
+                                else{
+                                    print("success")
+                                }
+                            }
+                            
+                        } label: {
+                            Image("\(alternateAppIcons[item])-Preview")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(16)
+                                .buttonStyle(.borderless)
+                    }
+                    }
+                }
+
+            } //: SCROLLVIEW
+            .padding(.top, 12)
+                Text("Choose your favorite app icon from the collection abobe.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom, 12)
+            } //: SECTION
+        header:  {
+                Text("Alternate Icon")
+            }
+               .listRowSeparator(.hidden)
+
             // MARK: - SECTION: ABOUT
             
             Section {
